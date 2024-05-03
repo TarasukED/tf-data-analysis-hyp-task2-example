b0ed251a6c4843af5fc76c9d22ccee8c16f4e0ec
TarasukED <160397770+TarasukED@users.noreply.github.com>
2024-05-03 20:48:34 +0300

    solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ks_2samp
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1742143281 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    stat, p_value = ks_2samp(sample1, sample2)
+    alpha = 0.04
+    return p_value < alpha
